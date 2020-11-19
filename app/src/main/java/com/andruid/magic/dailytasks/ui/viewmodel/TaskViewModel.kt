445c6bd36631056fc658f1439907884f22269a1b
DeaDshot14 <mistry14darren@gmail.com>
2020-11-20 01:17:25 +0530

    updated edit text cursor color

--- app/src/main/java/com/andruid/magic/dailytasks/ui/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/andruid/magic/dailytasks/ui/viewmodel/TaskViewModel.kt
@@ -1,18 +1,14 @@
 package com.andruid.magic.dailytasks.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.cachedIn
+import androidx.paging.liveData
 import com.andruid.magic.dailytasks.database.TaskRepository
 
 class TaskViewModel : ViewModel() {
-    private val pager = Pager(
+    val tasksLiveData = Pager(
         PagingConfig(pageSize = 10),
         pagingSourceFactory = { TaskRepository.getTasks() }
-    )
-
-    val tasksLiveData = pager.flow.cachedIn(viewModelScope).asLiveData()
+    ).liveData
 }
